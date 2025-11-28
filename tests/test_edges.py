6675fb13a7ddecb468c6f95686e1ca0f96848ff2
elb29 <lbihan.etienne@gmail.com>
2025-11-28 14:46:23 +0100

    adapt test edges

--- tests/test_edges.py
+++ tests/test_edges.py
@@ -5,19 +5,18 @@ from shapely import Polygon
 def test_edges():
 
     edges_computer = pyscoring.metrics.edges.EdgesMetric()
-    assert edges_computer.compute() == (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    assert edges_computer.compute() == (0.0, 0.0, 0.0, 0.0, 0, 0)
 
     pred = [Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])]
     gt = [Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])]
 
     edges_computer.update(pred, gt)
-    assert edges_computer.compute() == (0, 0, 0, 0, 0, 0, 0, 0, 1, 1)
-
+    assert edges_computer.compute() == (0.0, 0.0, 0.0, 0.0, 1, 1)
     edges_computer.update(pred, gt)
-    assert edges_computer.compute() == (0, 0, 0, 0, 0, 0, 0, 0, 2, 2)
+    assert edges_computer.compute() == (0, 0, 0, 0, 2, 2)
 
     edges_computer.reset()
-    assert edges_computer.compute() == (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    assert edges_computer.compute() == (0.0, 0.0, 0.0, 0.0, 0, 0)
 
     pred = [Polygon([(0.25, 0), (1.25, 0), (1.25, 1), (0.25, 1)])]
     gt = [Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])]
@@ -28,10 +27,6 @@ def test_edges():
         0.25,
         0.25,
         0.25,
-        11.25,
-        11.25,
-        90.0,
-        90.0,
         1,
         1,
     )
